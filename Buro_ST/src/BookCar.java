a53bfbd19d0bb10201acd2e00d0c07b465d3a817
Denis Kutnár <deni2346@stud.kea.dk>
2018-11-27 21:59:39 +0100

    Book car - object

---
+++ Buro_ST/src/BookCar.java
@@ -0,0 +1,66 @@
+public class BookCar {
+
+    private String location;
+    private String make;
+    private String model;
+    private String avaStart;
+    private String avaEnd;
+    private int price;
+
+    public BookCar(String location, String make, String model, String avaStart, String avaEnd, int price) {
+        this.location = location;
+        this.make = make;
+        this.model = model;
+        this.avaStart = avaStart;
+        this.avaEnd = avaEnd;
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getAvaStart() {
+        return avaStart;
+    }
+
+    public void setAvaStart(String avaStart) {
+        this.avaStart = avaStart;
+    }
+
+    public String getAvaEnd() {
+        return avaEnd;
+    }
+
+    public void setAvaEnd(String avaEnd) {
+        this.avaEnd = avaEnd;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
